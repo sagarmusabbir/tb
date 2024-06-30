e967d628d09e1a71c2ce3f356210cb9e7c0352c7
MUSABBIR SAGAR <56739377+sagarmusabbir@users.noreply.github.com>
2024-07-01 02:25:32 +0600

    Update default.tsx

--- components/default.tsx
+++ components/default.tsx
@@ -27,7 +27,7 @@ import {
   PiSunBold,
 } from "react-icons/pi";
 import { DefaultDrawer } from "./drawer";
-import MenuBar, { MoonIcon, SunIcon } from "./icons";
+import Bar, { MoonIcon, SunIcon } from "./icons";
 import Image from "next/image";
 import Logo from "./icons";
 
@@ -66,7 +66,7 @@ export function HeaderNavigation() {
           </Button>
         </Link>
 
-        <NavbarToggle barIcon={MenuBar} />
+        <NavbarToggle barIcon={Bar} />
       </div>
       <NavbarCollapse className=" items-center">
         <NavLink />
